9eee0fae6ec0f02a53420a1e1f5ebe904c7e43ed
Mike "Ozzie" Osmonson <mikeosmonson@gmail.com>
2025-02-10 16:32:56 -0700

    Add refactore naming

--- src/Library.js
+++ src/Library.js
@@ -1,3 +1,5 @@
+// const { unshelfBook } = require('../src/shelf');
+
 function createLibrary(name) {
   let library = {
     name: name,
@@ -18,17 +20,19 @@ function checkShelfIndex(shelf, title) {
   return shelf.findIndex(book => book.title === title);
 }
 
-function checkoutBook(library, title, genre) {
-  let index = checkShelfIndex(library.shelves[genre], title)
+function checkoutBook(library, bookTitle, genre) {
+  let index = checkShelfIndex(library.shelves[genre], bookTitle)
   if (index !== -1) {
+    // unshelfBook(bookTitle, library.shelves[genre]);
     library.shelves[genre].splice(index, 1);
-    return `You have now checked out ${title} from the ${library.name}`
+    return `You have now checked out ${bookTitle} from the ${library.name}`
   }
-  return `Sorry, there are currently no copies of ${title} available at the ${library.name}`
+  return `Sorry, there are currently no copies of ${bookTitle} available at the ${library.name}`
 }
 
 module.exports = {
   createLibrary,
   addBook,
-  checkoutBook
+  checkoutBook,
+  checkShelfIndex
 };
