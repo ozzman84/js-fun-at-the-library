66d6ed272616a4d2623a8de91a47acdd1914c6cd
Mike "Ozzie" Osmonson <mikeosmonson@gmail.com>
2025-02-10 16:15:46 -0700

    Add function to unshelf books

--- src/shelf.js
+++ src/shelf.js
@@ -1,12 +1,19 @@
+const { checkShelfIndex } = require('../src/library');
+
 function shelfBook(book, shelf) {
   if (shelf.length < 3) {
     shelf.unshift(book);
   }
 }
 
+function unshelfBook(bookTitle, shelf) {
+  let index = checkShelfIndex(shelf, bookTitle);
+  shelf.splice(index, 1);
+}
+
 module.exports = {
   shelfBook,
-  // unshelfBook,
+  unshelfBook,
   // listTitles,
   // searchShelf
 };
